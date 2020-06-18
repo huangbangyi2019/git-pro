eb5c4b6089ed9b394cd5e08604a2fc159396ac14
huangbangyi2019 <1094476088@qq.com>
2020-06-18 09:21:43 +0800

    提交

---
+++ git-pro/src/com/nnxy/team5/Wangkai.java
@@ -0,0 +1,18 @@
+package com.nnxy.team5;
+
+public class Wangkai {
+    private int id;
+    private int age;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
