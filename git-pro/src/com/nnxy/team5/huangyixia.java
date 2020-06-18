4aba2692139811e62b560af34a71165ac8660804
huangbangyi2019 <1094476088@qq.com>
2020-06-18 09:43:17 +0800

    黄已夏提交

---
+++ git-pro/src/com/nnxy/team5/huangyixia.java
@@ -0,0 +1,32 @@
+package com.nnxy.team5;
+
+public class huangyixia {
+	
+	private int id;
+	private String name;
+	private String sex;
+	
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	
+
+}
