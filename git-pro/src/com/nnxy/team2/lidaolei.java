493b914a94621a522e5ead5431426beba32dd057
huangbangyi2019 <1094476088@qq.com>
2020-06-18 10:57:34 +0800

    add

---
+++ git-pro/src/com/nnxy/team2/lidaolei.java
@@ -0,0 +1,14 @@
+package com.nnxy.team2;
+
+public class lidaolei {
+	private int name;
+
+	public int getName() {
+		return name;
+	}
+
+	public void setName(int name) {
+		this.name = name;
+	}
+     
+}
