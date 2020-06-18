a5f4b626d68f50b0a74a4ca19eb252e971c44336
huangbangyi2019 <1094476088@qq.com>
2020-06-18 09:22:19 +0800

    第一次提交到本地仓库

---
+++ git-pro/src/com/nnxy/team2/zhoujunye.java
@@ -0,0 +1,25 @@
+package com.nnxy.team2;
+
+public class zhoujunye {
+   int name;
+   int sex;
+   int number;
+public int getName() {
+	return name;
+}
+public void setName(int name) {
+	this.name = name;
+}
+public int getSex() {
+	return sex;
+}
+public void setSex(int sex) {
+	this.sex = sex;
+}
+public int getNumber() {
+	return number;
+}
+public void setNumber(int number) {
+	this.number = number;
+}
+}
