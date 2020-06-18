04d352a31f0833d87148ac6c2517cdf810e35ea7
huangbangyi2019 <1094476088@qq.com>
2020-06-18 09:31:48 +0800

    黄锦金提交

--- git-pro/src/com/nnxy/team3/Huangjinjin.java
+++ git-pro/src/com/nnxy/team3/Huangjinjin.java
@@ -4,45 +4,29 @@ public class Huangjinjin {
 	private Integer id;
 	private String name;
 	private String school;
-	
-
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getSchool() {
 		return school;
 	}
-
 	public void setSchool(String school) {
 		this.school = school;
 	}
-
-	
 	@Override
 	public String toString() {
 		return "Huangjinjin [id=" + id + ", name=" + name + ", school=" + school + "]";
 	}
 	
-	Huangjinjin(Integer id,String name,String school)
-	{
-		System.out.println("学号"+id);
-		System.out.println("姓名"+name);
-		System.out.println("学校"+school);
-	}
-
+	
 
 }
