b98ddb5aefb02f9fc243c8ebec0f0acad1271745
huangbangyi2019 <1094476088@qq.com>
2020-06-24 10:52:25 +0800

    064

--- git-pro/src/com/nnxy/team2/Caoshaoguang.java
+++ git-pro/src/com/nnxy/team2/Caoshaoguang.java
@@ -1,8 +1,7 @@
 package com.nnxy.team2;
 
 public class Caoshaoguang {
-	
-	
-	private int id;
-
+	public static void main(String[] args) {
+		System.out.println("我是第二组的 曹绍广!/n20170217064/17物联网工程4班");
+	}
 }
